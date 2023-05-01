4fd6e82f0545f03c4e9791ac8b4b37d8b2327f8f
Sanket Sabnavis <122606987+sanketsabnavis@users.noreply.github.com>
2023-05-01 16:16:56 +0530

    Add files via upload

---
+++ n1.py
@@ -0,0 +1,16 @@
+import random
+secretnum=random.randint(1,20)
+print("Im thinking of a num bitween 1 and 20")
+for guessestaken in range(1,6):
+    print("take a guess")
+    guess=int(input("guess: "))
+    if guess < secretnum:
+        print("ur guess is too low...!")
+    elif guess > secretnum:
+        print("ur guess is too high")
+    else:
+        break
+if guess == secretnum:
+    print("Good job! u guessed my num in" + str(guessestaken) + "guess!")
+else:
+    print("Nope...! the num is was thinking was" + str(secretnum))
